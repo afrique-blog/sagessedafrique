29f898d581b19b9b77df2117749d069113c6999c
Marco <marco@sagessedafrique.com>
2026-02-01 12:39:55 +0300

    feat(editor): Add Markdown auto-formatting to TinyMCE

--- frontend/components/RichTextEditor.tsx
+++ frontend/components/RichTextEditor.tsx
@@ -45,6 +45,24 @@ export default function RichTextEditor({
           'bold italic forecolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | ' +
           'link image media | blockquote | removeformat | code | help',
+        // Markdown auto-formatting patterns
+        text_patterns: [
+          { start: '# ', format: 'h1' },
+          { start: '## ', format: 'h2' },
+          { start: '### ', format: 'h3' },
+          { start: '#### ', format: 'h4' },
+          { start: '* ', cmd: 'InsertUnorderedList' },
+          { start: '- ', cmd: 'InsertUnorderedList' },
+          { start: '1. ', cmd: 'InsertOrderedList' },
+          { start: '> ', format: 'blockquote' },
+          { start: '---', replacement: '<hr />' },
+          { start: '`', end: '`', format: 'code' },
+          { start: '**', end: '**', format: 'bold' },
+          { start: '*', end: '*', format: 'italic' },
+          { start: '__', end: '__', format: 'bold' },
+          { start: '_', end: '_', format: 'italic' },
+          { start: '~~', end: '~~', format: 'strikethrough' },
+        ],
         content_style: `
           body { 
             font-family: Georgia, Cambria, serif; 
